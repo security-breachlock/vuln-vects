6c139c29863ac7d236d3588e9f4707e5d5dd470f
Saul Johnson <saul.a.johnson@gmail.com>
2021-08-01 21:11:44 +0200

    Add validation convenience methods

--- src/index.ts
+++ src/index.ts
@@ -12,13 +12,15 @@ export { enums as cvss3 } from "./cvss3-enums";
 export { CvssScore } from "./cvss-score";
 export { Cvss2ScoringEngine } from "./cvss2-scoring-engine";
 export { Cvss2VectorParser } from "./cvss2-vector-parser";
+export { Cvss2VectorMocker } from "./cvss2-vector-mocker";
 export { Cvss3ScoringEngine } from "./cvss3-scoring-engine";
 export { Cvss3VectorParser } from "./cvss3-vector-parser";
+export { Cvss3VectorMocker } from "./cvss3-vector-mocker";
 export { MultiCvssVectorParser } from "./multi-cvss-vector-parser";
 
 
 /**
- *  Parses a CVSS v2 vector and returns the resulting score object.
+ * Parses a CVSS v2 vector and returns the resulting score object.
  *
  * @param vector the vector to parse
  * @returns the resulting score object
@@ -29,7 +31,7 @@ export function parseCvss2Vector(vector: string): CvssScore {
 }
 
 /**
- *  Parses a CVSS v3 vector and returns the resulting score object.
+ * Parses a CVSS v3 vector and returns the resulting score object.
  *
  * @param vector the vector to parse
  * @returns the resulting score object
@@ -40,7 +42,7 @@ export function parseCvss3Vector(vector: string): CvssScore {
 }
 
 /**
- *  Parses a CVSS vector (any version) and returns the resulting score object.
+ * Parses a CVSS vector (any version) and returns the resulting score object.
  *
  * @param vector the vector to parse
  * @returns the resulting score object
@@ -48,4 +50,49 @@ export function parseCvss3Vector(vector: string): CvssScore {
 export function parseCvssVector(vector: string): CvssScore {
     let parser = new MultiCvssVectorParser();
     return parser.parse(vector);
+}
+
+/**
+ * Validates a CVSS v2 vector.
+ *
+ * @param vector the vector to parse
+ * @returns true if validation succeeded, otherwise false
+ */
+ export function validateCvss2Vector(vector: string): boolean {
+    try {
+        parseCvss2Vector(vector);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+/**
+ * Validates a CVSS v3.x vector.
+ *
+ * @param vector the vector to parse
+ * @returns true if validation succeeded, otherwise false
+ */
+export function validateCvss3Vector(vector: string): boolean {
+    try {
+        parseCvss3Vector(vector);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+/**
+ *  Parses a CVSS vector (any version).
+ *
+ * @param vector the vector to parse
+ * @returns true if validation succeeded, otherwise false
+ */
+export function validateCvssVector(vector: string): boolean {
+    try {
+        parseCvssVector(vector);
+        return true;
+    } catch (e) {
+        return false;
+    }
 }
